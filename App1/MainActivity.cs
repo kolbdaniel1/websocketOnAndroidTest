df5e2b6b1a36e27517d69be67a37804a03a742ef
Daniel Kolb <kolbdaniel1@gmail.com>
2015-11-06 18:08:37 +0100

    initial commit

---
+++ App1/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        WebsocketService websocket;
+ 
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button connect = FindViewById<Button>(Resource.Id.conn);
+            Button send1 = FindViewById<Button>(Resource.Id.send);
+            Button sendMultiple = FindViewById<Button>(Resource.Id.mul);
+            websocket = new WebsocketService();
+
+        }
+
+        private void mulmj(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1000; i++) { 
+            websocket.Send("");
+        }
+        }
+
+        private void send(object sender, EventArgs e)
+        {
+            websocket.Send("asdf");
+        }
+
+        private void conn(object sender, EventArgs e)
+        {
+             websocket.ConnectWithWebsocket();
+        }
+    }
+}
+
---
+++ App1/WebsocketService.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebSocket.Portable.Interfaces;
+using WebSocket.Portable;
+using dralloMultiPlayer.Messages;
+
+namespace App1
+{
+    public class WebsocketService
+    {
+        private const string webSocketURI = "ws://drallodmmprototype.azurewebsites.net/dmmSocketConnector.ashx";
+
+        public event Action Closed;
+        public event Action Error;
+        public event Action<string> Received;
+        private const string challengeId = "45435-2435-245-2345-234";
+        private const string deviceId = "123123123";
+        private const string userName = "colbinator";
+        WebSocketClient connection;
+
+        public WebsocketService()
+        {
+            connection = new WebSocketClient();
+            connection.Closed += OnWebsocketClosed;
+            connection.Error += OnWebsocketError;
+            connection.MessageReceived += OnWebsocketReceived;
+            connection.AutoSendPongResponse = false;
+            
+        }
+
+
+        public void ConnectWithWebsocket()
+        {
+            try
+            {
+               
+
+                connection.OpenAsync(webSocketURI);
+            }
+            catch (Exception e)
+            {
+
+                Debug.WriteLine("could not connect: " + e.Message);
+            }
+        }
+
+        void OnWebsocketClosed()
+        {
+            Debug.WriteLine("Closed Websocket");
+            Closed();
+        }
+
+
+        void OnWebsocketError(Exception e)
+        {
+            Debug.WriteLine("Websocket Error" + e.Message);
+            Error();
+        }
+
+        public async Task Send(string msg)
+        {
+
+            try
+            {
+                EchoWithTimestamp echo = new EchoWithTimestamp(msg);
+                var msgFrame = new MessageFrame("echo-request", echo);
+                string message = JsonConvert.SerializeObject(msgFrame);
+
+                Debug.WriteLine("SEND" + message);
+                await connection.SendAsync(message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("exception in Send(): " + e.Message);
+            }
+
+        }
+
+        public async Task Register()
+        {
+            var registerMsg = new RegisterMessage();
+            registerMsg.challengeId = challengeId;
+            registerMsg.deviceId = deviceId;
+            var registerMessage = new MessageFrame("register", registerMsg);
+
+            string message = JsonConvert.SerializeObject(registerMessage);
+
+            await connection.SendAsync(message);
+            Debug.WriteLine("sent register message: " + message);
+        }
+
+        public async Task Deregister()
+        {
+            var deregisterMsg = new DeregisterMessage();
+            deregisterMsg.challengeId = challengeId;
+            deregisterMsg.deviceId = deviceId;
+            var deregisterMessage = new MessageFrame("deregister", deregisterMsg);
+
+            string message = JsonConvert.SerializeObject(deregisterMessage);
+
+            await connection.SendAsync(message);
+
+            Debug.WriteLine("sent deregister message");
+        }
+
+        public async Task Join()
+        {
+            var joinMsg = new JoinMessage();
+            joinMsg.challengeId = challengeId;
+            joinMsg.userName = userName;
+            var joinMessage = new MessageFrame("join", joinMsg);
+
+            string message = JsonConvert.SerializeObject(joinMessage);
+            await connection.SendAsync(message);
+
+            Debug.WriteLine("sent join message");
+        }
+
+        int numberOfReceivedMessages = 0;
+        void OnWebsocketReceived(IWebSocketMessage message)
+        {
+
+            string msgstr = message.ToString();
+            Debug.WriteLine("recevid message:" + msgstr);
+
+            var messageFrame = JsonConvert.DeserializeObject<MessageFrame>(msgstr);
+            object typedMessage = new object();
+
+            switch (messageFrame.messageType)
+            {
+                case "invite":
+                    typedMessage = JsonConvert.DeserializeObject<JoinMessage>(messageFrame.data.ToString());
+                    break;
+                case "echo-request":
+                    typedMessage = JsonConvert.DeserializeObject<EchoWithTimestamp>(messageFrame.data.ToString());
+                    break;
+                case "echo-reply":
+                    typedMessage = JsonConvert.DeserializeObject<EchoWithTimestamp>(messageFrame.data.ToString());
+                    break;
+                default:
+                    throw new Exception("invalid message type");
+            }
+
+            Debug.WriteLine(typedMessage.ToString() + " received ");
+            //numberOfReceivedMessages++;
+            //Received(msgstr);
+        }
+    }
+}
+
